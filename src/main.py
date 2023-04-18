0d1e97a7296b5a08e94539bc73791cdb8676ce0b
v.marsakov <v.marsakov@artistrazh.tech>
2023-04-18 16:31:55 +0300

    fix validate input data

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from models import news as news_model
 from database import get_async_session
 from datetime import date, timedelta
 from schemas import NewsInfo, NewsPeriod
-from typing import List
+from typing import List, Annotated
 
 app = FastAPI(
     title="API MosMetro"
@@ -18,15 +18,9 @@ async def start_page() -> str:
 
 
 @app.get("/metro/news")
-async def get_last_news(day: int = 0, session: AsyncSession = Depends(get_async_session)) -> List[NewsInfo]:
+async def get_last_news(day: Annotated[int, Query(le=14, ge=0)],
+                        session: AsyncSession = Depends(get_async_session)) -> List[NewsInfo]:
     """ Запрос на получение новостей за прошедшие n дней. Максимальное кол-во прошедших дней 14 """
-    if day < 0:  # валидация на неотрицательное число
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Number of days elapsed cannot be negative",
-        )
-    elif day > 14:  # дополнительно ограничение (максимум выдавать новости за последние 14 дней)
-        day = 14
     current_date = date.today()
     before_date_news = current_date - timedelta(days=day)
     query = select(news_model.c.title, news_model.c.date, news_model.c.url_picture).filter(news_model.c.date.between(
@@ -40,17 +34,6 @@ async def get_profile_id_by_username(period: NewsPeriod, session: AsyncSession =
         -> List[NewsInfo]:
     """ Дополнительный запрос для получения новостей за определенный период времени.
         Максимальный период 30 дней."""
-    # дополнительно сделал ограничения на кол-во дней по которым будет выдавать новости
-    if period.end_period < period.start_period:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="End date of the period is less than the Start date",
-        )
-    elif (period.end_period - period.start_period).days > 30:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="The limit of the maximum number of days in the period has been exceeded. Max count day 30!",
-        )
     query = select(news_model.c.title, news_model.c.date, news_model.c.url_picture).filter(news_model.c.date.between(
         period.start_period, period.end_period))
     news = await session.execute(query)
--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field
 from datetime import date
 from typing import Optional
 
@@ -10,5 +10,14 @@ class NewsInfo(BaseModel):
 
 
 class NewsPeriod(BaseModel):
-    start_period: date = date.today()
-    end_period: date = date.today()
+    start_period: date = Field(default_factory=date.today)
+    end_period: date = Field(default_factory=date.today)
+
+    @validator('end_period', always=True)
+    def passwords_match(cls, v, values):
+        if v < values.get('start_period'):
+            raise ValueError("End date of the period is less than the Start date")
+        elif (v - values.get('start_period')).days > 30:
+            raise ValueError("The limit of the maximum number of days in the period has been exceeded. "
+                             "Max count day 30!")
+        return v
